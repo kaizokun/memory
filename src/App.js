fd8e4dc54b0283ee73690173173e2c357af3d9e1
monsio <monsio.b@gmail.com>
2018-10-31 11:36:33 +0100

    first commit events

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import './App.css'
+
+import Card from './Card'
+import GuessCount from './GuessCount'
+
+
+
+class App extends Component {
+
+    handleCardClick(card) {
+        console.log(card, 'clicked')
+    }
+
+    render() {
+        return (
+            <div className="memory">
+                <GuessCount guesses={0} />
+                <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+                <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+                <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
+                <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+                <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+                <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+            </div>
+        )
+    }
+}
+export default App
