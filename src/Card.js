d58a8055f5464a4dcb954be1551715364e1f2906
monsio <monsio.b@gmail.com>
2018-10-31 12:05:00 +0100

    test methode handleclick dans class Card

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,35 @@
-import React from 'react'
-
+import React, {Component} from 'react'
 import './Card.css'
 
 const HIDDEN_SYMBOL = '❓'
 
-const Card = ({ card, feedback, onClick }) => (
-    <div className={`card ${feedback}`} onClick={() => onClick(card)}>
-        <span className="symbol">
-            {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
-        </span>
-    </div>
-)
+class Card extends Component {
+
+    constructor (props) {
+
+        super(props)
+
+        this.card = props.card;
+
+        this.feedback = props.feedback;
+    }
+
+    render(){
+
+        return(
+            <div className={`card ${this.feedback}`} onClick={() => this.handleCardClick()}>
+                <span className="symbol">
+                    {this.feedback === 'hidden' ? HIDDEN_SYMBOL : this.card}
+                </span>
+            </div>
+        )
+    }
+
+    handleCardClick() {
+
+        console.log(this.card, 'clicked')
+    }
+
+}
 
 export default Card
